ff915ba1948e328b0a9a2153c39aae5ce40337c4
Karun Kumar <36824081+karun02525@users.noreply.github.com>
2023-05-03 01:47:08 +0530

    Update views.py

--- product/views.py
+++ product/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Product,Book,Category
-from .serializers import ProductSerializer,BookSerializer,CategorySerializer,UserSerializer,UserLoginSerializer
+from .serializers import ProductSerializer,BookSerializer,CategorySerializer,UserSerializer
 from  rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
